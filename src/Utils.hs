b82d03743665c789dbcd268642a85bac5e092f96
Camto <www.ben.pr@gmail.com>
2020-05-29 02:21:58 -0400

    Add embed color.

--- src/Utils.hs
+++ src/Utils.hs
@@ -10,6 +10,10 @@ import qualified Calamity.Commands.Context as CC
 import qualified Calamity.HTTP.Channel as C
 import qualified Calamity.Types.Model.Channel.Embed as C
 
+import qualified Data.Word as W
+
+embed_color = fromInteger 0xe07bb8 :: W.Word64
+
 embed_options embed = C.CreateMessageOptions {
 	C.content = Nothing,
 	C.nonce = Nothing,
