aa3b2eee6331e243d3f659dcb4d9a1c09d8524a2
Camto <www.ben.pr@gmail.com>
2020-05-30 00:39:50 -0400

    Add absolutely garbage fuzzy matching to `l.dino`

--- src/Cmds/Dino.hs
+++ src/Cmds/Dino.hs
@@ -7,6 +7,7 @@ import qualified Calamity.HTTP.Channel as C
 import qualified Calamity.Cache.Eff as C
 import qualified Calamity.Metrics.Eff as C
 
+import Debug.Trace
 import Control.Monad
 import Control.Lens
 import Data.Maybe
@@ -15,6 +16,7 @@ import qualified Data.Text.Lazy as L
 import qualified Data.Aeson as AS
 import qualified Data.HashMap.Strict as HM
 import qualified Data.FuzzySet as F
+import qualified Safe as S
 
 import qualified Polysemy as P
 import qualified Polysemy.Embed as P
@@ -33,14 +35,16 @@ dino ::
 
 dino icons emojis dinos =
 	C.command @'[Maybe L.Text] "dino" $ \ctx -> \case
-		Just dino' -> dino_cmd icons emojis dinos ctx $ fromMaybe default_dino Nothing
+		Just dino' ->
+			dino_cmd icons emojis dinos ctx . L.fromStrict $
+				fromMaybe default_dino . fmap snd $
+					S.atMay (F.get dino_set $ L.toStrict dino') 0
 		Nothing ->
 			(P.embed $ U.pick_one_hm dinos) >>=
 			dino_cmd icons emojis dinos ctx . L.fromStrict
 	where
 		dino_set = F.fromList $ HM.keys dinos
-		default_dino :: L.Text
-		default_dino = L.fromStrict $ HM.keys dinos !! 0
+		default_dino = HM.keys dinos !! 0
 
 dino_cmd icons emojis dinos ctx dino' =
 	void $ U.send_text (ctx ^. #channel) dino'
